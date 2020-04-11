e0d0b390f7f2d614c8382e54361a77e0b53091ff
delchev <delchevn@gmail.com>
2020-04-11 22:54:11 +0300

    on save model serialiser

---
+++ ide-entity/template/transformer-on-save.js
@@ -0,0 +1,23 @@
+var transformer = require("ide-entity/template/transform-edm");
+var workspaceManager = require("workspace/v4/manager");
+
+var workspace = __context.get('workspace');
+var project = __context.get('project');
+var path = __context.get('path');
+
+var modelPath = path.replace(".edm", ".model");
+var content = transformer.transform(workspace, project, path);
+
+if (content !== null) {
+    var bytes = require("io/v4/bytes");
+    input = bytes.textToByteArray(content);
+
+    if (workspaceManager.getWorkspace(workspace)
+        .getProject(project).getFile(path).exists()) {
+            workspaceManager.getWorkspace(workspace)
+                .getProject(project).createFile(modelPath, input);
+    } else {
+        workspaceManager.getWorkspace(workspace)
+            .getProject(project).getFile(modelPath).setContent(input);
+    }
+}
