05490526249e5dd50f2bdb24a8482d5b91092796
ThuF <jordan.p.pavlov@gmail.com>
2022-09-08 12:45:19 +0300

    Adoption of new template generation

--- ide-entity/services/transformer-template.js
+++ ide-entity/services/transformer-template.js
@@ -1,3 +1,16 @@
+const transformer = require("ide-entity/template/transform-edm");
+
+exports.generate = function (model, parameters) {
+    let workspaceName = parameters.workspaceName;
+    let projectName = parameters.projectName;
+    let filePath = parameters.filePath;
+    let fileName = filePath.substring(0, filePath.indexOf(".edm"));
+    return [{
+        path: `${fileName}.model`,
+        content: transformer.transform(workspaceName, projectName, filePath)
+    }]
+};
+
 exports.getTemplate = function () {
     let template = {
         "name": "Entity Data to JSON Model Transformer",
@@ -7,10 +20,11 @@ exports.getTemplate = function () {
             {
                 "location": "/ide-entity/template/source.model.template",
                 "action": "generate",
-                "rename": "{{fileNameBase}}.model",
+                "rename": "{{projectName}}.model",
                 "engine": "javascript",
                 "handler": "/ide-entity/template/transformer.js"
-            }]
+            }],
+        "parameters": []
     };
     return template;
 }
